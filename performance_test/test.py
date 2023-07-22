b2c2fec924a7e214bdc5428634aa4ee64013eaae
satoshiakatsuka <tsukasa7613@gmail.com>
2023-07-22 23:24:44 +0900

    Add: execution_speed decorator
    実行速度比較用デコレータを追記

--- performance_test/test.py
+++ performance_test/test.py
@@ -1,6 +1,34 @@
+import time
 import pandas as pd
 import polars as pl
 
+def execution_speed(func):
+    """実行速度計測用のデコレータ
+    """
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"関数名:{func.__name__} 実行時間:{run_time}秒")
+    return wrapper
+
+
 def main():
-    pd_df = pd.read_csv("../docs/user_list.csv")
-    print(pd_df.last)
+    """CSVの読み込み速度を比較
+    """
+    file_path = r"docs\user_list_10000.csv"
+    pandas_read_csv(file_path)
+    polars_read_csv(file_path)
+
+
+@execution_speed
+def pandas_read_csv(path: str):
+    user_list = pd.read_csv(path)
+    print(user_list.tail(3))
+
+
+@execution_speed
+def polars_read_csv(path: str):
+    user_list = pl.read_csv(path)
+    print(user_list.tail(3))
